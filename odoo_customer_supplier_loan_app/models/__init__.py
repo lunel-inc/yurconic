077cc02441268fdfe1ef05cedc0cf1a90019294a
James Dominy <jamesdominy@lunelerp.com>
2021-01-21 07:04:45 -0700

    [RST] Reset repo to base

--- odoo_customer_supplier_loan_app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import loan_proof_type
-from . import loan_request
-from . import loan_installment
-from . import inherited_partner
-from . import account_payment
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
